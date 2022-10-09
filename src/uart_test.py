4a86a355c67ff25ed6e9468f1aebe0abe2d4eefa
kalchaar <keenan.alchaar@gmail.com>
2022-10-08 22:40:58 -0400

    uart_test.py added

---
+++ src/uart_test.py
@@ -0,0 +1,17 @@
+import serial
+from time import sleep
+
+ser = serial.Serial(
+    port="/dev/ttyS0", 
+    baudrate = 9600, 
+    parity=serial.PARITY_NONE, 
+    stopbits=serial.STOPBITS_ONE, 
+    bytesize=serial.EIGHTBITS,
+)
+while True:
+    received_data = ser.read()
+    sleep(1)
+    data_left = ser.inWaiting()
+    received_data += ser.read(data_left)
+    print(received_data)
+    print("...")
