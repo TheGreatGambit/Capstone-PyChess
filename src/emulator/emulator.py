d0a5968f385d93973cea7fd28484c1f897bedc4d
DarkenedDragon <eridor314@gmail.com>
2022-10-31 19:28:20 -0400

    Implemented a super basic emulator

--- src/emulator/emulator.py
+++ src/emulator/emulator.py
@@ -1,1 +1,19 @@
 import serial
+
+com_port = 'COM16'  # Change this
+
+# Sends the moves from the terminal to the MSP
+with serial.Serial(com_port, 9600) as ser:
+    while True:
+        try:
+            user_input = input("Move (ex: e2e4): ")
+            user_input = user_input.strip().lower()
+            # TODO: Add input checking
+
+            message = bytes(user_input + 'M', 'utf-8')
+            ser.write(message)
+            print(f'Sent {message}')
+        except KeyboardInterrupt:
+            print('\n')
+            break
+    print("Exiting")
